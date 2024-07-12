139faf7b73486af8168321292d186b2e4a5b4228
VadimTolstov <tolstov50@mail.ru>
2024-07-12 18:13:13 +0300

    v-0.01

---
+++ src/main/java/mail/tolstov/v/e/TestSpring.java
@@ -0,0 +1,13 @@
+package mail.tolstov.v.e;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
