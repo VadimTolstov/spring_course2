f084ed8e290bc788c914c62b12ba604674517d55
VadimTolstov <tolstov50@mail.ru>
2024-07-14 20:15:37 +0300

    v-0.01

--- src/main/java/mail/tolstov/v/e/MusicPlayer.java
+++ src/main/java/mail/tolstov/v/e/MusicPlayer.java
@@ -2,18 +2,23 @@ package mail.tolstov.v.e;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 @Component
 public class MusicPlayer {
-
+    @Value(value = "${musicPlayer2.name}")
+    private String name;
+    @Value(value = "${musicPlayer2.volume}")
+    private int volume;
     private Music music1;
     private Music music2;
 
     @Autowired
-    public MusicPlayer(@Qualifier("classicalMusic") Music music1, @Qualifier("musicBean") Music music2) {
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("musicBean") Music music2) {
         this.music1 = music1;
         this.music2 = music2;
     }
@@ -26,5 +31,19 @@ public class MusicPlayer {
 
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 }
